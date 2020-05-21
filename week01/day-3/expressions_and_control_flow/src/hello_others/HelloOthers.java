da8f3ae4bca9909d20df5aab09d351a1b92fa8ad
greszter <esztergreskovics@gmail.com>
2020-05-21 14:34:25 +0200

    add .gitignore and update hello_me, hello_others, humpty_dumpty

---
+++ week01/day-3/expressions_and_control_flow/src/hello_others/HelloOthers.java
@@ -0,0 +1,17 @@
+package hello_others;
+
+public class HelloOthers {
+    public static void main(String[] args) {
+        // Greet 3 of your classmates with this program, in three separate lines
+        // like:
+        //
+        // Hello, Esther!
+        // Hello, Mary!
+        // Hello, Joe!
+
+        System.out.println("Hello Szid!");
+        System.out.println("Hello Aron!");
+        System.out.println("Hello Dani!");
+        System.out.println("Hello everybody else!");
+    }
+}
