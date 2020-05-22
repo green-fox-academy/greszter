0c641d2c0253824e0fd2f63a64cad16b736e4006
greszter <esztergreskovics@gmail.com>
2020-05-22 17:28:04 +0200

    add draw_triangle

---
+++ week01/day-3/expressions_and_control_flow/src/draw_triangle/DrawTriangle.java
@@ -0,0 +1,34 @@
+package draw_triangle;
+
+import java.util.Scanner;
+
+public class DrawTriangle {
+    public static void main(String[] args) {
+        // Write a program that reads a number from the standard input, then draws a
+        // triangle like this:
+        //
+        // *
+        // **
+        // ***
+        // ****
+        //
+        // The triangle should have as many lines as the number was
+
+        Scanner scanner = new Scanner(System.in);
+        String a = "*";
+
+        System.out.println("Add a number:");
+        int userNumber = scanner.nextInt();
+
+        for (int i = 1; i <= userNumber; i++) {
+            for (int j = 1; j <= userNumber; j++) {
+                if (j <= i){
+                System.out.print("*");}
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+
