5dfba2dfe7028f46df73c8d5658c76390f58ec28
greszter <esztergreskovics@gmail.com>
2020-05-22 15:15:41 +0200

    add i_wont_cheat_on_the_exams

---
+++ week01/day-3/expressions_and_control_flow/src/i_wont_cheat_on_the_exams/IWontCheatOnTheExams.java
@@ -0,0 +1,16 @@
+package i_wont_cheat_on_the_exams;
+
+public class IWontCheatOnTheExams {
+    public static void main(String[] args) {
+        // Create a program that writes this line 100 times:
+        // "I won't cheat on the exam!"
+
+        int a = 0;
+
+        while (a < 100) {
+            a++;
+            System.out.println(a + ". I won't cheat on the exam!");
+
+        }
+    }
+}
