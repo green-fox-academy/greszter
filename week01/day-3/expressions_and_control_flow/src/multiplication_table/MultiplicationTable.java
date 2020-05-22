2ecfd3b8053bb25096c44cd6b7714aa17cbaf978
greszter <esztergreskovics@gmail.com>
2020-05-22 15:31:30 +0200

    add multiplication_table

---
+++ week01/day-3/expressions_and_control_flow/src/multiplication_table/MultiplicationTable.java
@@ -0,0 +1,35 @@
+package multiplication_table;
+
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        // Create a program that asks for a number and prints the multiplication table with that number
+        //
+        // Example:
+        // The number 15 should print:
+        //
+        // 1 * 15 = 15
+        // 2 * 15 = 30
+        // 3 * 15 = 45
+        // 4 * 15 = 60
+        // 5 * 15 = 75
+        // 6 * 15 = 90
+        // 7 * 15 = 105
+        // 8 * 15 = 120
+        // 9 * 15 = 135
+        // 10 * 15 = 150
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Give a number:");
+
+        int userInput = scanner.nextInt();
+        int a = 1;
+
+        while (a <= 10){
+            System.out.println(a + " * " + userInput + " = " + (a * userInput));
+            a++;
+        }
+
+    }
+}
