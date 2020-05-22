9f6add2b6594091dc410f5c653ab59eac0df9854
greszter <esztergreskovics@gmail.com>
2020-05-22 13:04:02 +0200

    add party_indicator

---
+++ week01/day-3/expressions_and_control_flow/src/party_indicator/PartyIndicator.java
@@ -0,0 +1,51 @@
+package party_indicator;
+
+import java.util.Scanner;
+
+public class PartyIndicator {
+    public static void main(String[] args) {
+        // Write a program that asks for two numbers
+        // The first number represents the number of girls that comes to a party, the
+        // second the boys
+        // It should print: The party is excellent!
+        // If the the number of girls and boys are equal and 20 or more people are coming to the party
+        //
+        // It should print: Quite cool party!
+        // If there are 20 or more people coming to the party but the girl - boy ratio is not 1-1
+        //
+        // It should print: Average party...
+        // If there are less people coming than 20
+        //
+        // It should print: Sausage party
+        // If no girls are coming, regardless the count of the people
+
+        System.out.println("Who comes to the party?");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Number of girls:");
+        int userInput1 = scanner.nextInt();
+        System.out.println("Number of boys:");
+        int userInput2 = scanner.nextInt();
+
+        boolean equal = (userInput1 == userInput2);
+        boolean notEqual = (userInput1 != userInput2);
+        int sum = userInput1 + userInput2;
+
+        if (equal){
+            if (sum > 20){
+                System.out.println("The party is excellent!");
+            } else if (sum < 20){
+                System.out.println("Average party...");
+            }
+        } else if (notEqual){
+            if (userInput1 == 0){
+                System.out.println("Sausage party");
+            } else if (sum > 20){
+                System.out.println("Quite cool party!");
+            } else if (sum < 20){
+                System.out.println("Average party...");
+            }
+        }
+    }
+}
+
