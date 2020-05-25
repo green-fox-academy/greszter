95ba4a4369700722f86a5c046843e97c3efa2a43
greszter <esztergreskovics@gmail.com>
2020-05-25 16:24:08 +0200

    add append_a

---
+++ week02/day-1/arrays/src/append_a/AppendA.java
@@ -0,0 +1,15 @@
+package append_a;
+
+public class AppendA {
+    public static void main(String[] args) {
+        // - Create an array variable named `animals`
+        //   with the following content: `["koal", "pand", "zebr"]`
+        // - Add all elements an `"a"` at the end
+
+        String[] animals = {"koal", "pand", "zebr"};
+
+        for (int i = 0; i < animals.length; i++) {
+            System.out.println(animals[i] + "a");
+        }
+    }
+}
