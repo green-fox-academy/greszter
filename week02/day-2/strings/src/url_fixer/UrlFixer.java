651ca35760ff1d57ec5d8f371ded1b018230eb99
greszter <esztergreskovics@gmail.com>
2020-05-26 16:55:10 +0200

    modify url_fixer

--- week02/day-2/strings/src/url_fixer/UrlFixer.java
+++ week02/day-2/strings/src/url_fixer/UrlFixer.java
@@ -9,8 +9,12 @@ public class UrlFixer {
         // Accidentally I got the wrong URL for a funny subreddit. It's probably "odds" and not "bots"
         // Also, the URL is missing a crucial component, find out what it is and insert it too!
         // Try to solve it more than once using different String functions!
-        
-        System.out.println(url.replace("bots", "odds/"));
+
+        String newurl = url.replace("bots", "odds");
+        StringBuilder urlFix = new StringBuilder(newurl);
+        urlFix.insert(newurl.indexOf("//"), ":");
+
+        System.out.println(urlFix.toString());
 
 
     }
