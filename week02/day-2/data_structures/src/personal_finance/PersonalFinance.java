a237d368f819c8b38fa49391ea2389acd2931948
greszter <esztergreskovics@gmail.com>
2020-05-26 17:10:51 +0200

    add personal_finance

---
+++ week02/day-2/data_structures/src/personal_finance/PersonalFinance.java
@@ -0,0 +1,30 @@
+package personal_finance;
+
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+public class PersonalFinance {
+    public static void main(String[] args) {
+        ArrayList<Integer> finances = new ArrayList<>();
+
+        Collections.addAll(finances, 500, 1000, 1250, 175, 800, 120);
+
+        int sum = 0;
+
+        for (int i = 0; i < finances.size(); i++){
+            sum = (sum + finances.get(i));
+        }
+        System.out.println("We spent: " + sum);
+
+        System.out.println("Our greatest expense was: " + Collections.max(finances));
+
+        System.out.println("Our cheapest spending was: " + Collections.min(finances));
+
+        System.out.println("Average amount of our spendings: " + ((double) sum / finances.size()));
+
+
+    }
+}
