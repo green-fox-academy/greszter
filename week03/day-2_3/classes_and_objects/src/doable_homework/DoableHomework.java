d92e7b303ea590fbe4a8f38fe748bb31c4b9bcf1
greszter <esztergreskovics@gmail.com>
2020-06-01 08:37:24 +0200

    add doable_homework

---
+++ week03/day-2_3/classes_and_objects/src/doable_homework/DoableHomework.java
@@ -0,0 +1,40 @@
+package doable_homework;
+
+import java.util.HashMap;
+
+public class DoableHomework {
+    public static void main(String[] args) {
+
+        HashMap<String, String> oak = new HashMap<>();
+        oak.put("type", "oak");
+        oak.put("leaf color", "green");
+        oak.put("age", "80-150 years");
+        oak.put("sex", "female");
+
+        HashMap<String, String> maple = new HashMap<>();
+        maple.put("type", "maple");
+        maple.put("leaf color", "green");
+        maple.put("age", "150-200 years");
+        maple.put("sex", "male");
+
+        HashMap<String, String> ash = new HashMap<>();
+        ash.put("type", "ash");
+        ash.put("leaf color", "green");
+        ash.put("age", "80-100 years");
+        ash.put("sex", "female");
+
+        HashMap<String, String> birch = new HashMap<>();
+        birch.put("type", "birch");
+        birch.put("leaf color", "green");
+        birch.put("age", "100-150 years");
+        birch.put("sex", "female");
+
+        HashMap<String, String> olive = new HashMap<>();
+        olive.put("type", "oak");
+        olive.put("leaf color", "green");
+        olive.put("age", "300-600 years");
+        olive.put("sex", "male");
+
+
+    }
+}
