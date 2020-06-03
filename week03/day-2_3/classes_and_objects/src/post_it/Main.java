6a9a25bfffe3db59ea9c69580169628d6b7b8e2d
greszter <esztergreskovics@gmail.com>
2020-06-03 13:55:07 +0200

    add post_it

---
+++ week03/day-2_3/classes_and_objects/src/post_it/Main.java
@@ -0,0 +1,13 @@
+package post_it;
+
+import javafx.geometry.Pos;
+
+public class Main {
+    public static void main(String[] args) {
+
+        PostIt orangePostIt = new PostIt("orange", "Idea 1", "blue");
+        PostIt pinkPostIt = new PostIt("pink", "Awesome", "black");
+        PostIt yellowPostIt = new PostIt("yellow", "Superb", "green");
+
+    }
+}
---
+++ week03/day-2_3/classes_and_objects/src/post_it/PostIt.java
@@ -0,0 +1,16 @@
+package post_it;
+
+public class PostIt {
+
+    private String backgroundColor;
+    private String text;
+    private String textColor;
+
+    public PostIt(String backgroundColor, String text, String textColor) {
+        this.backgroundColor = backgroundColor;
+        this.text = text;
+        this.textColor = textColor;
+
+
+    }
+}
