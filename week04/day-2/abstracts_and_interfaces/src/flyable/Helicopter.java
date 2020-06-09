bc42a0673dd3e507dc67750b38cf4bbbe6894d7b
greszter <esztergreskovics@gmail.com>
2020-06-09 16:21:42 +0200

    add flyable and implement flyable to Bird in zoo

---
+++ week04/day-2/abstracts_and_interfaces/src/flyable/Helicopter.java
@@ -0,0 +1,20 @@
+package flyable;
+
+public class Helicopter extends Vehicle implements Flyable{
+
+
+    @Override
+    public void land() {
+        System.out.println("Landing");
+    }
+
+    @Override
+    public void fly() {
+        System.out.println("Flying");
+    }
+
+    @Override
+    public void takeOff() {
+        System.out.println("Taking off");
+    }
+}
---
+++ week04/day-2/abstracts_and_interfaces/src/flyable/Vehicle.java
@@ -0,0 +1,9 @@
+package flyable;
+
+public abstract class Vehicle {
+
+    protected int fuelAmount;
+    protected int speed;
+    protected String color;
+
+}
