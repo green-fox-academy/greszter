116a0d4ac153f6f39b80622abc3fa5738b9d5d89
greszter <esztergreskovics@gmail.com>
2020-06-29 14:24:26 +0200

    add exercise4 in streams

---
+++ week06/day1/stream/src/exercise4/Exercise4.java
@@ -0,0 +1,22 @@
+package exercise4;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Exercise4 {
+  public static void main(String[] args) {
+//    Write a Stream Expression to get the average value of the odd numbers from the following list:
+
+    List<Integer> numbers = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14);
+
+
+    double result = numbers.stream()
+        .filter(n -> n % 2 == 1 || n % 2 == -1)
+        .mapToInt(n -> n)
+        .average()
+        .orElse(0);
+
+    System.out.println(result);
+
+  }
+}
