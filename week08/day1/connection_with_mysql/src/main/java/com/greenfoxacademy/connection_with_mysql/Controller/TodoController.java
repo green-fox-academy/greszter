0f2b5b2716d4cfb569cd7435f23412910e72b918
greszter <esztergreskovics@gmail.com>
2020-07-13 15:50:47 +0200

    update Connection with MySQL with add new todo feature

--- week08/day1/connection_with_mysql/src/main/java/com/greenfoxacademy/connection_with_mysql/Controller/TodoController.java
+++ week08/day1/connection_with_mysql/src/main/java/com/greenfoxacademy/connection_with_mysql/Controller/TodoController.java
@@ -1,10 +1,13 @@
 package com.greenfoxacademy.connection_with_mysql.Controller;
 
+import com.greenfoxacademy.connection_with_mysql.Model.Todo;
 import com.greenfoxacademy.connection_with_mysql.Repository.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class TodoController {
@@ -24,9 +27,21 @@ public class TodoController {
 
   @GetMapping(value = {"/", "/list"})
   public String list(Model model){
-    model.addAttribute("todos", todoRepository.findAllByDoneIsFalse());
+    model.addAttribute("todos", todoRepository.findAll());
 
     return "todolist";
+  }
+
+  @GetMapping("/addtodo")
+  public String add(Model model){
+    model.addAttribute("newtodo", new Todo());
+    return "addtodo";
+  }
+
+  @PostMapping("/addtodo")
+  public String addNew(@ModelAttribute Todo todo, String title){
+    todoRepository.save(new Todo(title));
+    return "redirect:/list";
   }
 
 
