32be0a7cd7dbcfd677279cff77c19b9d53a06ba9
greszter <esztergreskovics@gmail.com>
2020-07-13 16:45:38 +0200

    update Connection with MySQL with delete feature

--- week08/day1/connection_with_mysql/src/main/java/com/greenfoxacademy/connection_with_mysql/Controller/TodoController.java
+++ week08/day1/connection_with_mysql/src/main/java/com/greenfoxacademy/connection_with_mysql/Controller/TodoController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -44,5 +45,11 @@ public class TodoController {
     return "redirect:/list";
   }
 
+  @GetMapping("/{id}/delete")
+  public String deleteItem(@PathVariable (name = "id") Long id, Model model){
+    todoRepository.deleteById(id);
+    model.addAttribute("todos", todoRepository.findAll());
+    return "redirect:/list";
+  }
 
 }
